96e47196d1d287570d6a11dab4144d0573599373
Lucas Reiter <lucas.reiter@paytrack.com.br>
2022-01-26 21:49:50 -0300

    Aula 02 - Implementando o design do item de transferência

--- bytebank/lib/main.dart
+++ bytebank/lib/main.dart
@@ -2,6 +2,24 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
       home: Scaffold(
+        body: Column(
+          children: const [
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.monetization_on),
+                title: Text('1000.00'),
+                subtitle: Text('8133280'),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.monetization_on),
+                title: Text('1234.00'),
+                subtitle: Text('8133280'),
+              ),
+            ),
+          ],
+        ),
         appBar: AppBar(
           title: Text('Transferências'),
         ),
